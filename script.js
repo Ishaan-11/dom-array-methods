0a51b471b867e10315b3c889c3efcb659db94472
Ishaan-11 <ishaansharma1998@gmail.com>
2020-12-12 19:32:25 +0530

    complete script file

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const main = document.getElementById('main');
 const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
 
 let data = [];
 
@@ -19,6 +23,38 @@ async function fetchUser() {
   };
 
   addUser(newUser);
+}
+
+// Double eveyones money
+function doubleMoney() {
+  data = data.map(item => {
+    return { ...item, money: item.money * 2}
+  });
+
+  updateDom();
+}
+
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter(item => item.money > 1000000);
+
+  updateDom();
+}
+
+// Sort users by richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+
+  updateDom();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+  const total = data.reduce((sum, item) => (sum += item.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(total)}</strong></h3>`;
+  main.appendChild(wealthEl);
 }
 
 // Add new obj to data arr
@@ -46,6 +82,9 @@ function formatMoney(money) {
   return '$' + money.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
-
 // event listners
 addUserBtn.addEventListener('click', fetchUser);
+doubleBtn.addEventListener('click', doubleMoney);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+sortBtn.addEventListener('click', sortByRichest);
+calculateWealthBtn.addEventListener('click', calculateWealth);
